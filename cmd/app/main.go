387b3c487fc2cbae664729c429f2827a8aa528cb
pak sem <p4ck.sem@yandex.ru>
2025-09-29 21:10:47 +0300

    change port 3000 on 8081

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -25,7 +25,7 @@ func main() {
 
 	// TODO: Разобраться как и для каких ручек настроить CORS
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "http://89.208.208.203:8081")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	})
